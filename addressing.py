12d819b455d06ddde5e71164a6f2f6cc647b1494
S.Mohammad Mousavi <smmousavisp@gmail.com>
2020-05-19 22:07:29 +0430

    Add a function to change ip to binary format

--- addressing.py
+++ addressing.py
@@ -11,3 +11,17 @@ def ip_string_to_ip_list(ip_string):
     :rtype: list
     """
     return [int(x) for x in ip_string.split('.')]
+
+
+def ip_list_to_ip_binary(ip_list):
+    """ Change list of parts of an IP to binary.
+
+    :param ip_list: List of parts of an IP
+    :type ip_list: list
+    :return: List of each part of an IP in binary
+    :rtype: list
+    """
+    ip_binary = [[] for _ in range(4)]
+    for i, part in enumerate(ip_list):
+        ip_binary[i] = [bool(x) for x in [int(y) for y in f"{part:b}"]]
+    return ip_binary
